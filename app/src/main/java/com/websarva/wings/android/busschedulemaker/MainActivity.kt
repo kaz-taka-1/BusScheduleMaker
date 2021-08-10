0ad8e59117e1890f5b38c025ae5e98074203fc33
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-08-10 16:42:31 +0900

    新規時刻表のダイアログのデザイン変更

--- app/src/main/java/com/websarva/wings/android/busschedulemaker/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/busschedulemaker/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.websarva.wings.android.busschedulemaker
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.AdapterView
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -20,10 +22,13 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    //オプションメニューがクリックされた時のメンバクラス
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         var returnVal =true
         when(item.itemId){
+            //新規時刻表がクリックされた時
             R.id.new_schedule -> {
+
                 val dialogFragment = NewScheduleDialog()
                 dialogFragment.show(supportFragmentManager,"NewScheduleDialog")
             }
--- app/src/main/java/com/websarva/wings/android/busschedulemaker/NewScheduleDialog.kt
+++ app/src/main/java/com/websarva/wings/android/busschedulemaker/NewScheduleDialog.kt
@@ -2,21 +2,31 @@ package com.websarva.wings.android.busschedulemaker
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ClipData
 import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import java.lang.IllegalStateException
+import kotlin.math.E
+
 
 class NewScheduleDialog:DialogFragment() {
+    //新規時刻表ダイアログの表示
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = activity?.let{
             val builder = AlertDialog.Builder(it)
-            builder.setTitle(R.string.tv_new_schedule)
+            /*builder.setTitle(R.string.tv_new_schedule)
             builder.setMessage(R.string.tv_title)
+            //builder.setView()
             builder.setMessage(R.string.tv_destination)
             builder.setMessage(R.string.tv_bus_stop)
             builder.setMessage(R.string.tv_operational_hour)
+            builder.setNegativeButton(R.string.bt_thx_ng,DialogButtonClickListener())
+            builder.setNegativeButton(R.string.bt_thx_ok,DialogButtonClickListener())*/
+            val inflater = requireActivity().layoutInflater;
+            builder.setView(inflater.inflate(R.layout.new_busstop_dialog, null))
             builder.setNegativeButton(R.string.bt_thx_ng,DialogButtonClickListener())
             builder.setNegativeButton(R.string.bt_thx_ok,DialogButtonClickListener())
             builder.create()
