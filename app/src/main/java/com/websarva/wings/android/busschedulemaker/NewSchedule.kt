4a69831081b6ee44694e46fd339d975158787451
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-08-16 15:18:32 +0900

    時刻表の作成用レイアウトファイルの作成

--- app/src/main/java/com/websarva/wings/android/busschedulemaker/NewSchedule.kt
+++ app/src/main/java/com/websarva/wings/android/busschedulemaker/NewSchedule.kt
@@ -15,30 +15,28 @@ class NewSchedule : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_schedule)
         val bt1 = findViewById<Button>(R.id.bt1)
-        val listener = screencreate()
         bt1.setOnClickListener(screencreate())
 
         //val wrapContent = ViewGroup.LayoutParams.WRAP_CONTENT
         //val linearLayout = findViewById<LinearLayout>(R.id.newshedule)
         //val editText = EditText(applicationContext)
-        /*val text1 = intent.getStringExtra("TEXT1")
-        val tvText1 = findViewById<TextView>(R.id.tvText1)
-        tvText1.text = text1*/
-
         //linearLayout.addView(editText, LinearLayout.LayoutParams(wrapContent,wrapContent))
 
     }
-
+    //ボタンクリック
     private inner class screencreate(): View.OnClickListener{
         override fun onClick(view: View?) {
             val etTitle = findViewById<EditText>(R.id.title).text.toString()
             val etDestination = findViewById<EditText>(R.id.destination).text.toString()
             val etBusStop = findViewById<EditText>(R.id.bus_stop).text.toString()
-            val etFirstTrainTime = findViewById<EditText>(R.id.first_train_time).text.toString()
-            val etLastTrainTime = findViewById<EditText>(R.id.last_train_time).text.toString()
+            val etFirstTrainTime = findViewById<EditText>(R.id.first_train_time)
+            val etLastTrainTime = findViewById<EditText>(R.id.last_train_time)
 
         }
 
     }
+
+    //時刻表の自動作成
+
 
 }
