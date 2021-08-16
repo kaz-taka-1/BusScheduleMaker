35759624b84649e9246a5f8b61d39e45553cd8b4
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-08-16 14:47:38 +0900

    新規時刻表画面部品の取得

--- app/src/main/java/com/websarva/wings/android/busschedulemaker/NewSchedule.kt
+++ app/src/main/java/com/websarva/wings/android/busschedulemaker/NewSchedule.kt
@@ -1,7 +1,9 @@
 package com.websarva.wings.android.busschedulemaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
@@ -12,14 +14,30 @@ class NewSchedule : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_schedule)
+        val bt1 = findViewById<Button>(R.id.bt1)
+        val listener = screencreate()
+        bt1.setOnClickListener(screencreate())
+
         //val wrapContent = ViewGroup.LayoutParams.WRAP_CONTENT
         //val linearLayout = findViewById<LinearLayout>(R.id.newshedule)
         //val editText = EditText(applicationContext)
-        val text1 = intent.getStringExtra("TEXT1")
+        /*val text1 = intent.getStringExtra("TEXT1")
         val tvText1 = findViewById<TextView>(R.id.tvText1)
-        tvText1.text = text1
+        tvText1.text = text1*/
 
         //linearLayout.addView(editText, LinearLayout.LayoutParams(wrapContent,wrapContent))
+
+    }
+
+    private inner class screencreate(): View.OnClickListener{
+        override fun onClick(view: View?) {
+            val etTitle = findViewById<EditText>(R.id.title).text.toString()
+            val etDestination = findViewById<EditText>(R.id.destination).text.toString()
+            val etBusStop = findViewById<EditText>(R.id.bus_stop).text.toString()
+            val etFirstTrainTime = findViewById<EditText>(R.id.first_train_time).text.toString()
+            val etLastTrainTime = findViewById<EditText>(R.id.last_train_time).text.toString()
+
+        }
 
     }
 
