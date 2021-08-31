c5d8eaae9d18347b9c98ecbe786663af06c770ae
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-08-31 11:52:50 +0900

    時刻表切り替え機能実装

--- app/src/main/java/com/websarva/wings/android/busschedulemaker/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/busschedulemaker/MainActivity.kt
@@ -14,85 +14,16 @@ import androidx.core.view.ViewCompat
 
 class MainActivity : AppCompatActivity() {
 
-    var busScheduleId:Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
-        val _helper = DatabaseHelper(this@MainActivity)
-        val db = _helper.writableDatabase
-        var sql = "SELECT * FROM busschedules WHERE _id = ${busScheduleId}"
-        val cursor =db.rawQuery(sql,null)
-        val tvTitle = findViewById<Spinner>(R.id.tvTitle)
-        tvTitle.setOnItemSelectedListener(ItemSelectedListener())
-        createSpinner(db)
-
-        var destination = ""
-        var busStop = ""
-        var firstTrainTime = 0
-        var lastTrainTime = 0
-
         val context = this
         val view1 = findViewById<View>(R.id.main)
         val a = aaa(context,view1)
-        a.call1()
-
-        a.call2()
-
-        /*while(cursor.moveToNext()){
-            val idxDestination = cursor.getColumnIndex("destination")
-            destination = cursor.getString(idxDestination)
-            val idxBusStop = cursor.getColumnIndex("busStop")
-            busStop = cursor.getString(idxBusStop)
-            val idxFirstTrainTime = cursor.getColumnIndex("firstTrainTime")
-            firstTrainTime = cursor.getInt(idxFirstTrainTime)
-            val idxLastTrainTime = cursor.getColumnIndex("lastTrainTime")
-            lastTrainTime = cursor.getInt(idxLastTrainTime)
-        }
-        //tvTitle.setText(title)
-        val tvDestination = findViewById<TextView>(R.id.tvDestination)
-        tvDestination.setText(destination)
-        val tvBusStop = findViewById<TextView>(R.id.tvBusStop)
-        tvBusStop.setText(busStop)*/
-        /*val tableLayout = findViewById<View>(R.id.timetable) as ViewGroup
-        tableLayout.removeAllViews()
-        var count = 0
-
-
-
-
-        var timeTableId = busScheduleId
-        val sql2 = "SELECT * FROM timetable WHERE _id = ${timeTableId}"
-        val cursor2 =db.rawQuery(sql2,null)
-        val minutes:MutableList<String> = mutableListOf()
-        minutes.add("0")
-        var minute = ""
-
-        while (cursor2.moveToNext()) {
-            for(i in 1 until 24) {
-                val clock = "o" + i.toString()
-                minute = ""
-                val idxminute = cursor2.getColumnIndex(clock)
-                minute = cursor2.getString(idxminute)
-                minutes.add(minute)
-            }
-        }
-        firstTrainTime = 1
-        lastTrainTime = 23
-        for (i in  firstTrainTime until (lastTrainTime + 1) ) {
-            val view = getLayoutInflater().inflate(R.layout.output_layout, tableLayout)
-            val tr = tableLayout.getChildAt(count) as TableRow
-            var tvHour = ((tr.getChildAt(0)) as TextView)
-            tvHour.setText(i.toString())
-            val evMinute = ((tr.getChildAt(1)) as TextView)
-            val tvminute = view.findViewById<TextView>(R.id.tvminute)
-            tvminute.setText(minutes[i])
-            tvminute.setId(ViewCompat.generateViewId())
-            count++
-        }*/
+        a.call3()
+        val tvTitle = findViewById<Spinner>(R.id.tvTitle)
+        tvTitle.setOnItemSelectedListener(ItemSelectedListener(context,view1))
 
     }
 
@@ -116,119 +47,146 @@ class MainActivity : AppCompatActivity() {
         return returnVal
     }
 
-    class ItemSelectedListener() :AdapterView.OnItemSelectedListener {
+    class ItemSelectedListener(context: Context,view1: View) :AdapterView.OnItemSelectedListener {
+        val context = context
+        val view1 = view1
+        var busScheduleId = 1
+
         override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
 
             val tvid = view?.findViewById<TextView>(R.id.tvIdItem)
             val id = tvid?.text.toString()
-            Log.i("table",id)
-            //aaa(db)
+
+            busScheduleId = id.toInt()
+            //var b = bbb(context,view1)
+
+            busScheduleId
+            call1()
+            call2()
         }
+
 
         override fun onNothingSelected(parent: AdapterView<*>) {
         }
-    }
+            val _helper = DatabaseHelper(context)
+            val db = _helper.writableDatabase
 
+            var destination = ""
+            var busStop = ""
+            var firstTrainTime = 0
+            var lastTrainTime = 0
 
-    private fun createSpinner(db:SQLiteDatabase){
-        val recodeCount = DatabaseUtils.queryNumEntries(db, "busschedules")
-        val splist2: MutableList<MutableMap<String,String>> = mutableListOf()
-        var title = ""
-        var id =""
-        val titles:MutableList<String> = mutableListOf()
-        val ids:MutableList<String> = mutableListOf()
-        for (i in 1 until recodeCount+1) {
-            val sql1 = "SELECT * FROM busschedules WHERE _id = ${i}"
-            val cursor1 =db.rawQuery(sql1,null)
-            while(cursor1.moveToNext()) {
-                val idnum = cursor1.getColumnIndex("_id")
-                val idxTitle = cursor1.getColumnIndex("title")
-                title = cursor1.getString(idxTitle)
-                id = cursor1.getString(idnum)
-                titles.add(title)
-                ids.add(id)
-                //var splist= mutableMapOf("id" to id.toString(),"title" to title)
-                //splist2.add(splist)
-                Log.i("table", titles.toString())
-                Log.i("table", ids.toString())
+            fun call1() {
+                Log.i("table", busScheduleId.toString())
+                Log.i("table","call1")
+                var sql = "SELECT * FROM busschedules WHERE _id = ${busScheduleId}"
+                val cursor = db.rawQuery(sql, null)
+                while (cursor.moveToNext()) {
+                    val idxDestination = cursor.getColumnIndex("destination")
+                    destination = cursor.getString(idxDestination)
+                    val idxBusStop = cursor.getColumnIndex("busStop")
+                    busStop = cursor.getString(idxBusStop)
+                    val idxFirstTrainTime = cursor.getColumnIndex("firstTrainTime")
+                    firstTrainTime = cursor.getInt(idxFirstTrainTime)
+                    val idxLastTrainTime = cursor.getColumnIndex("lastTrainTime")
+                    lastTrainTime = cursor.getInt(idxLastTrainTime)
+                }
+                val tvDestination = view1.findViewById<TextView>(R.id.tvDestination)
+                tvDestination.setText(destination)
+                val tvBusStop = view1.findViewById<TextView>(R.id.tvBusStop)
+                tvBusStop.setText(busStop)
             }
 
-        }
-        val adapter = SpinnerAdapter(this.getApplicationContext(),
-            R.layout.spinner_item, ids,titles);
-        val tvTitle = findViewById<Spinner>(R.id.tvTitle)
-        tvTitle.adapter = adapter
+            fun call2(){
+                Log.i("table","call2")
+                val view2 = view1
+                val tableLayout = view1.findViewById<View>(R.id.timetable) as ViewGroup
+                tableLayout.removeAllViews()
+                var count = 0
+
+                var timeTableId = busScheduleId
+                val sql2 = "SELECT * FROM timetable WHERE _id = ${timeTableId}"
+                val cursor2 =db.rawQuery(sql2,null)
+                val minutes:MutableList<String> = mutableListOf()
+                minutes.add("0")
+                var minute = ""
+
+                while (cursor2.moveToNext()) {
+                    for(i in 1 until 24) {
+                        val clock = "o" + i.toString()
+                        minute = ""
+                        val idxminute:Int? = cursor2.getColumnIndex(clock)
+                        minute = idxminute?.let { cursor2.getString(it) }.toString()
+                        if (minute != null) {
+                            minutes.add(minute)
+                        }
+                    }
+                }
+
+                for (i in  firstTrainTime until (lastTrainTime + 1) ) {
+                    val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                    inflater.inflate(R.layout.output_layout, tableLayout)
+                    val tr = tableLayout.getChildAt(count) as TableRow
+                    var tvHour = ((tr.getChildAt(0)) as TextView)
+                    tvHour.setText(i.toString())
+                    val evMinute = ((tr.getChildAt(1)) as TextView)
+                    val tvminute = tableLayout.findViewById<TextView>(R.id.tvminute)
+                    tvminute.setText(minutes[i])
+                    tvminute.setId(ViewCompat.generateViewId())
+                    count++
+                }
+
+
+            }
+
+
+        //}
+
+
     }
 
-   private class aaa(context: Context,view1:View){
+
+   class aaa(context: Context,view1:View){
        val context = context
        val _helper = DatabaseHelper(context)
        val db = _helper.writableDatabase
-       var busScheduleId = 1
+       var busScheduleId = 2
        var destination = ""
        var busStop = ""
        var firstTrainTime = 0
        var lastTrainTime = 0
        val view1 = view1
-       fun call1() {
-            var sql = "SELECT * FROM busschedules WHERE _id = ${busScheduleId}"
-            val cursor = db.rawQuery(sql, null)
-            while (cursor.moveToNext()) {
-                val idxDestination = cursor.getColumnIndex("destination")
-                destination = cursor.getString(idxDestination)
-                val idxBusStop = cursor.getColumnIndex("busStop")
-                busStop = cursor.getString(idxBusStop)
-                val idxFirstTrainTime = cursor.getColumnIndex("firstTrainTime")
-                firstTrainTime = cursor.getInt(idxFirstTrainTime)
-                val idxLastTrainTime = cursor.getColumnIndex("lastTrainTime")
-                lastTrainTime = cursor.getInt(idxLastTrainTime)
-            }
-            val tvDestination = view1.findViewById<TextView>(R.id.tvDestination)
-            tvDestination.setText(destination)
-            val tvBusStop = view1.findViewById<TextView>(R.id.tvBusStop)
-            tvBusStop.setText(busStop)
-        }
-
-       fun call2(){
-           val view2 = view1
-           val tableLayout = view1.findViewById<View>(R.id.timetable) as ViewGroup
-           tableLayout.removeAllViews()
-           var count = 0
-
-           var timeTableId = busScheduleId
-           val sql2 = "SELECT * FROM timetable WHERE _id = ${timeTableId}"
-           val cursor2 =db.rawQuery(sql2,null)
-           val minutes:MutableList<String> = mutableListOf()
-           minutes.add("0")
-           var minute = ""
-
-           while (cursor2.moveToNext()) {
-               for(i in 1 until 24) {
-                   val clock = "o" + i.toString()
-                   minute = ""
-                   val idxminute = cursor2.getColumnIndex(clock)
-                   minute = cursor2.getString(idxminute)
-                   minutes.add(minute)
+       fun call3(){
+           Log.i("table","call3")
+           val recodeCount = DatabaseUtils.queryNumEntries(db, "busschedules")
+           val splist2: MutableList<MutableMap<String,String>> = mutableListOf()
+           var title = ""
+           var id =""
+           val titles:MutableList<String> = mutableListOf()
+           val ids:MutableList<String> = mutableListOf()
+           for (i in 1 until recodeCount+1) {
+               val sql1 = "SELECT * FROM busschedules WHERE _id = ${i}"
+               val cursor1 =db.rawQuery(sql1,null)
+               while(cursor1.moveToNext()) {
+                   val idnum = cursor1.getColumnIndex("_id")
+                   val idxTitle = cursor1.getColumnIndex("title")
+                   title = cursor1.getString(idxTitle)
+                   id = cursor1.getString(idnum)
+                   titles.add(title)
+                   ids.add(id)
+                   //var splist= mutableMapOf("id" to id.toString(),"title" to title)
+                   //splist2.add(splist)
+                   Log.i("table", titles.toString())
+                   Log.i("table", ids.toString())
                }
+
            }
-
-           for (i in  firstTrainTime until (lastTrainTime + 1) ) {
-               val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-               inflater.inflate(R.layout.output_layout, tableLayout)
-               val tr = tableLayout.getChildAt(count) as TableRow
-               var tvHour = ((tr.getChildAt(0)) as TextView)
-               tvHour.setText(i.toString())
-               val evMinute = ((tr.getChildAt(1)) as TextView)
-               val tvminute = tableLayout.findViewById<TextView>(R.id.tvminute)
-               tvminute.setText(minutes[i])
-               tvminute.setId(ViewCompat.generateViewId())
-               count++
-           }
-
-
+           val adapter = SpinnerAdapter(context, R.layout.spinner_item, ids,titles);
+           val tvTitle = view1.findViewById<Spinner>(R.id.tvTitle)
+           tvTitle.adapter = adapter
        }
 
-    }
+   }
 
 
 
